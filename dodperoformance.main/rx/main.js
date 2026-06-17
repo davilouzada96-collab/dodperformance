4912a07ea203b9f3e6f5df3eb7a75276d0de82a1
dodperformance <davilouzada96@gmail.com>
2026-06-17 10:24:00 -0300

    Add files via upload
    .github/
    assets/
    clinico/
    rx/
    index.html
    README.md
    _headers

---
+++ dodperoformance.main/rx/main.js
@@ -0,0 +1,11 @@
+import { initNavigation } from "./navigation.js";
+import { initAccordion } from "./ui/accordion.js";
+import { initTheme } from "./ui/theme.js";
+import { initResearchCards } from "./research-cards.js";
+import { initContactForm } from "./forms.js";
+
+initNavigation();
+initAccordion();
+initTheme();
+initResearchCards();
+initContactForm();
